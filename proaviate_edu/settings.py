74c4be74b0cd689d058d501f77991456f5963fb9
MilanStefanik <stefanik.milan@gmail.com>
2020-10-26 12:15:52 +0000

    Created base template and home template

--- proaviate_edu/settings.py
+++ proaviate_edu/settings.py
@@ -50,7 +50,7 @@ INSTALLED_APPS = [
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
-    'courses',
+    'home',
 
     # Other
     'storages',
@@ -71,7 +71,10 @@ ROOT_URLCONF = 'proaviate_edu.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
+            os.path.join(BASE_DIR, 'templates', 'allauth'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
