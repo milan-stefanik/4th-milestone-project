36ba1ba560a7339cdf2b1036948f1b99707647be
MilanStefanik <stefanik.milan@gmail.com>
2020-10-27 12:48:58 +0000

    Added contact page

--- home/views.py
+++ home/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.conf import settings
 
 # Create your views here.
 
@@ -7,3 +8,13 @@ def index(request):
     """ A view to return the index page """
 
     return render(request, 'home/index.html')
+
+
+def contact(request):
+    """ A view to return the index page """
+
+    context = {
+        'api_key': settings.GOOGLE_MAPS_API_KEY
+    }
+
+    return render(request, 'home/contact.html', context)
